fa42f7857a3ae5ae72bfc54dd764ee89c47694d7
joelmindset <82834522+joelmindset@users.noreply.github.com>
2021-05-11 16:44:00 -0600

    Project 2
    project 2

---
+++ Basic_JavaScrpt_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,14 @@
+function Adder() { // define fuction and name it
+    var a = 3; //adding nums
+    var b = 4;
+    var result = a + b; // adding the numbers
+    document.getElementById("add3_7").innerHTML = result;
+}
+
+function clicker() {
+    var set = "by clicking";
+    set += " also this to come"
+    document.getElementById("clicker1").innerHTML = set;
+}
+
+document.getElementById("p1").innertext = "New text!";
